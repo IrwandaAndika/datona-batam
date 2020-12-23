0fb5946cc6aeb9b776490c5750d8f84a3ed91ecd
IrwandaAndika <wandahd2908@gmail.com>
2020-12-23 15:40:10 +0700

    views for admin page

--- resources/views/dashboard/adminpage.blade.php
+++ resources/views/dashboard/adminpage.blade.php
@@ -18,7 +18,76 @@
     </div>
 @endsection
 @section('content')
-    <div class="container">
-        Hello World
+<div class="container">
+    @if (session('massage'))
+        <div class="alert alert-success alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h5 class="mt-2"><i class="icon fas fa-check"></i>{{ session('massage') }}</h5>
+        </div>
+    @endif
+    <div class="content">
+        <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+
+                <p class="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's
+                    content.
+                </p>
+
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+
+            <div class="card card-primary card-outline">
+                <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+
+                <p class="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's
+                    content.
+                </p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+                </div>
+            </div><!-- /.card -->
+            </div>
+            <!-- /.col-md-6 -->
+            <div class="col-lg-6">
+            <div class="card">
+                <div class="card-header">
+                <h5 class="card-title m-0">Featured</h5>
+                </div>
+                <div class="card-body">
+                <h6 class="card-title">Special title treatment</h6>
+
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+            <div class="card card-primary card-outline">
+                <div class="card-header">
+                <h5 class="card-title m-0">Featured</h5>
+                </div>
+                <div class="card-body">
+                <h6 class="card-title">Special title treatment</h6>
+
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            </div>
+            <!-- /.col-md-6 -->
+        </div>
+        <!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
+    <!-- /.content -->
+    </div>
+</div>
 @endsection
