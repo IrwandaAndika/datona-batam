c174bdc008f6c8ab6cd7d7d8409e884b04cccd65
IrwandaAndika <wandahd2908@gmail.com>
2020-12-23 15:34:19 +0700

    layouts for dashboard

--- resources/views/layouts/dashboard/admin-page.blade.php
+++ resources/views/layouts/dashboard/admin-page.blade.php
@@ -15,36 +15,21 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="{{asset('AdminLTE/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{asset('AdminLTE/dist/css/adminlte.min.css')}}">
+  <!-- TinyMCE --> 
+  <script src="https://cdn.tiny.cloud/1/06mlpkcw1azllwwelfv9at8oqcxrdf97jqyjhlx5oln60xfr/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
 
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <nav class="main-header navbar navbar-expand navbar-primary navbar-white">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
     </ul>
-
-    <!-- SEARCH FORM -->
-    <form class="form-inline ml-3">
-      <div class="input-group input-group-sm">
-        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-navbar" type="submit">
-            <i class="fas fa-search"></i>
-          </button>
-        </div>
-      </div>
-    </form>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
@@ -56,6 +41,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </a>
           <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right" aria-labelledby="navbarDropdown">
               <a href="#" class="dropdown-item">
+                  <a class="dropdown-item text-center" href="{{ route('admin.dashboard.profile') }}">
+                    <i class="fas fa-user-circle"></i> {{ _('Profile') }}
+                  </a>
+                  <hr>
                   <a class="dropdown-item text-center" href="{{ route('admin.logout') }}">
                     <i class="fas fa-sign-out-alt"></i> {{ _('logout') }}
                   </a>
@@ -80,7 +69,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   Brad Diesel
-                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                  <span class="float-right text-sm text- "><i class="fas fa-star"></i></span>
                 </h3>
                 <p class="text-sm">Call me whenever you can...</p>
                 <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
@@ -138,16 +127,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <span class="float-right text-muted text-sm">3 mins</span>
           </a>
           <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-file mr-2"></i> 3 new reports
-            <span class="float-right text-muted text-sm">2 days</span>
-          </a>
-          <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
         </div>
       </li>
@@ -176,15 +155,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
+      
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{asset('img/freddy.jpg')}}" class="img-square elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Freddy</a>
-        </div>
-      </div>
-
+        @if(Auth::user()->avatar)
+          <div class="image">
+          <img src="{{asset('/storage/img/'. Auth::user()->avatar)}}" class="img-square elevation-2" alt="User Image" width="100">
+          </div>    
+        @endif
+          <div class="info">
+              <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+          </div>
+      </div>   
+      
       <!-- SidebarSearch Form -->
       <div class="form-inline">
         <div class="input-group" data-widget="sidebar-search">
@@ -212,45 +194,95 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="/admin" class="nav-link active">
+                <a href="/admin" class="nav-link {{ request()->is('admin') ? 'active' : '' }}">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Admin Dashboard</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="/home" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Datona Home Page</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="/cases-page" class="nav-link">
+                <a href="/cases-page" class="nav-link {{ request()->is('cases-page') ? 'active' : '' }}">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Cases</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="/expertise-page" class="nav-link">
+                <a href="/expertises-page" class="nav-link {{ request()->is('expertises-page') ? 'active' : '' }}">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Expertise</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="/testimonials-page" class="nav-link">
+                <a href="/testimonials-page" class="nav-link {{ request()->is('testimonials-page') ? 'active' : '' }}">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Testimonials</p>
                 </a>
               </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon fas fa-book"></i>
+                  <p>
+                    Datona Home Page
+                    <i class="fas fa-angle-left right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="{{ url('/') }}" class="nav-link {{ request()->is('/') ? 'active' : '' }}">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Home</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ route('about') }}" class="nav-link {{ request()->is('about-us') ? 'active' : '' }}">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>About Us</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ route('expertise') }}" class="nav-link {{ request()->is('expertise') ? 'active' : '' }}">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Expertise</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ route('cases') }}" class="nav-link {{ request()->is('cases') ? 'active' : '' }}">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Cases</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ route('testimonials') }}" class="nav-link {{ request()->is('testimonials') ? 'active' : '' }}">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Testimonials</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ route('team') }}" class="nav-link {{ request()->is('team') ? 'active' : '' }}">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Team</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ route('partnerships') }}" class="nav-link {{ request()->is('partnerships') ? 'active' : '' }}">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Partnerships</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ route('gallery') }}" class="nav-link {{ request()->is('gallery') ? 'active' : '' }}">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Gallery</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ route('contact') }}" class="nav-link {{ request()->is('contact-us') ? 'active' : '' }}">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Contact Us</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
             </ul>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
           </li>
         </ul>
       </nav>
@@ -288,7 +320,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2014-2020 <a href="#">Datona Consulting</a>.</strong> All rights reserved.
   </footer>
 </div>
 <!-- ./wrapper -->
@@ -300,6 +332,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- Bootstrap 4 -->
 <script src="{{asset('AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset('dist/js/adminlte.min.js')}}"></script>
+<script src="{{asset('AdminLTE/dist/js/adminlte.min.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('AdminLTE/dist/js/demo.js')}}"></script>
 </body>
 </html>
