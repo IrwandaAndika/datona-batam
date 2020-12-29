56021b30bba66e5cbfd720e5e69d3134a5be16d6
IrwandaAndika <wandahd2908@gmail.com>
2020-12-29 09:32:08 +0700

    use class hash

--- app/Http/Controllers/Auth/AdminRegisterController.php
+++ app/Http/Controllers/Auth/AdminRegisterController.php
@@ -6,7 +6,7 @@ use App\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Auth\Hash;
+use Illuminate\Support\Facades\Hash;
 
 class AdminRegisterController extends Controller
 {
