cb3e23c1227f368c4b0619595e90c76bf2f2ec24
IrwandaAndika <wandahd2908@gmail.com>
2021-01-07 12:11:56 +0700

    add link

--- database/migrations/2021_01_07_050542_change_expertises_table_column.php
+++ database/migrations/2021_01_07_050542_change_expertises_table_column.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateExpertises extends Migration
+class ChangeExpertisesTableColumn extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreateExpertises extends Migration
      */
     public function up()
     {
-        Schema::create('expertises', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->string('sub_title');
-            $table->text('content');
-            $table->string('image');
-            $table->timestamps();
+        Schema::table('expertises', function (Blueprint $table) {
+            $table->renameColumn('sub_title', 'link');
         });
     }
 
@@ -30,6 +25,8 @@ class CreateExpertises extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('expertises');
+        Schema::table('expertises', function (Blueprint $table) {
+            $table->dropColumn(['link']);
+        });
     }
 }
