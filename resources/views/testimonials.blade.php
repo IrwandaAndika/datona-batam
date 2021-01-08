839787e11ff3fa726960bba32f175a39a910b1f6
IrwandaAndika <wandahd2908@gmail.com>
2021-01-08 10:28:04 +0700

    test 2

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -25,7 +25,7 @@
             @foreach ($testimonials as $t)
                 <div class="row mb-4 pb-4">
                     <div class="col-8 col-sm-4 col-lg-2 text-center pt-5">
-                    <img src="{{ asset('storage/app/public'. $t->image) }}" class="img-fluid custom-rounded-image"/>
+                    <img src="{{ asset('storage/app/public/'. $t->image) }}" class="img-fluid custom-rounded-image"/>
                     </div>
                     <div class="col-12 col-sm-12 col-lg-10">
                         <div class="testimonial custom-testimonial-style-1 testimonial-with-quotes mb-0">
